1606789457bd3dd9047606dc039ec1382454505f
monkgotdunk069-a11y <monkgotdunk069@gmail.com>
2025-10-18 12:15:25 +0530

    Update b_sort.py

--- tsc_sorting/b_sort.py
+++ tsc_sorting/b_sort.py
@@ -1,17 +1,15 @@
-#sorting_1
-arr = [ 2, 9, 1, 5, 6]
-high = len(arr)
-low=0
-key = int(input("Enter the key: "))
-mid = low + (high - low) // 2
-for i in range(len(arr)):
-    if arr[mid] == key:
-        print("Element found at index", mid)
-        break
-    elif arr[mid] < key:
-        low = mid + 1
-    elif arr[mid] > key:
-        high = mid - 1
-    mid = low + (high - low) // 2
-else:
-    print("Element not found in the array")
+#repeatedly steps through the list, compares adjacent elements and swaps them if they are in the wrong order
+def bs(arr,n):
+    for i in range(n):# Traverse through all array elements n-1 
+        for j in range(0,n-i-1):#upto n-2
+            if arr[j]>arr[j+1]:#swap if the element found is greate than the next element 
+                tem = arr[j]
+                arr[j]=arr[j+1]
+                arr[j+1]=tem
+    for i in range(n):
+        print(arr[i],end=" ")
+    
+if __name__ == "__main__":
+    arr=[64,34,25,12,22,11,90]
+    n=len(arr)
+    bs(arr,n)
